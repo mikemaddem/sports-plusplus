0279001bf2a747c4d7b5a439f77c40fd410230af
Mike M <mikemaddem@gmail.com>
2019-10-09 15:44:39 -0400

    basic pages models

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class StaticInfo(models.Model):
+    about = models.TextField(default='about us')
+    twitter = models.URLField(max_length=255, default='https://www.twitter.com')
+    facebook = models.URLField(max_length=255, default='https://www.facebook.com')
+    tos = models.TextField(default='tos')
+    privacy = models.TextField(default='privacy')
+
